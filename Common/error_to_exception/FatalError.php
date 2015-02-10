f3ad66321c138aab13e3d5b51ede4b06fc4d5287
Your Name <you@example.com>
2015-02-10 21:01:35 +0800

    -

--- Common/error_to_exception/FatalError.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Hyperframework\Common;
-
-class FatalError extends Error {
-    public function __construct($severity, $message, $file, $line) {
-        parent::__construct($severity, $message, $file, $line);
-    }
-}
